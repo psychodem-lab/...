b01d3e9daa3136359ff7482f4bd766a3551d13f3
psychodem-lab <psychologdemenczuk@gmail.com>
2026-04-19 23:37:29 +0200

    Create poziomy.js

---
+++ poziomy.js
@@ -0,0 +1,24 @@
+
+const gameLevels = [
+    {
+        letters: ['A', 'K', 'O', 'T', 'R'],
+        words: ["AKT", "KAT", "KOT", "KTO", "OKA", "TAK", "TOK", "KOTA", "RAK", "ROK", "TOR", "ORT", "KOR", "TAR", "ART", "TRA", "KORA", "ROTA", "TARO", "KART", "TORA", "TROK", "AKTOR"]
+    },
+    {
+        letters: ['E', 'K', 'R', 'A', 'N'],
+        words: ["ERA", "RAK", "AKR", "KAR", "KRA", "NAR", "RAN", "REN", "NER", "EKA", "AREK", "KARE", "RENA", "KERA", "EKRAN"]
+    },
+    {
+        letters: ['P', 'L', 'A', 'N', 'O'],
+        words: ["PAL", "ALP", "NAP", "PAN", "POL", "LOP", "OPAL", "PLAN", "PLON", "LANO", "ALON"]
+    },
+    {
+        letters: ['S', 'M', 'O', 'K', 'I'],
+        words: ["SOK", "KOS", "SOM", "MOS", "KIM", "MIK", "KOM", "SIK", "KIS", "SKI", "KOSI", "MOKI", "SMOK", "SKOM", "SOKI", "MIKO", "SMOKI"]
+    },
+    {
+        letters: ['B', 'R', 'A', 'K', 'I'],
+        words: ["BAK", "KAB", "BAR", "RAB", "ARB", "RAK", "AKR", "KAR", "KRA", "IRA", "KIR", "BIK", "BARK", "KRAB", "BRAK", "BAKI", "KARI", "RAKI", "KIRA", "BARI", "BIRA", "BRAKI"]
+    }
+    // Tutaj możesz dodawać kolejne poziomy według tego samego wzoru
+];
