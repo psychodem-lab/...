3c7a8ae5b0fa09525057f0f6fe05a268330d9884
psychodem-lab <psychologdemenczuk@gmail.com>
2026-04-19 23:47:10 +0200

    Update poziomy.js

--- poziomy.js
+++ poziomy.js
@@ -19,6 +19,46 @@ const gameLevels = [
     {
         letters: ['B', 'R', 'A', 'K', 'I'],
         words: ["BAK", "KAB", "BAR", "RAB", "ARB", "RAK", "AKR", "KAR", "KRA", "IRA", "KIR", "BIK", "BARK", "KRAB", "BRAK", "BAKI", "KARI", "RAKI", "KIRA", "BARI", "BIRA", "BRAKI"]
+    },
+    {
+        letters: ['E', 'K', 'R', 'A', 'N'],
+        words: ["ERA", "RAK", "KAN", "REK", "AREK", "ARKA", "ERKA", "EKAN", "KARE", "NERA", "AREN", "KRAN", "NERK", "EKRAN"]
+    },
+    {
+        letters: ['R', 'A', 'D', 'I', 'O'],
+        words: ["DAR", "DOA", "ORA", "RAD", "ROD", "ROI", "DORA", "ODRA", "DIOR", "ROID", "DRIO", "RADO", "RADIO"]
+    },
+    {
+        letters: ['T', 'R', 'A', 'W', 'A'],
+        words: ["RAT", "TAR", "WAR", "ARA", "ARTA", "RATA", "TARA", "WATA", "WARA", "TRWA", "TAWA", "TRAWA"]
+    },
+    {
+        letters: ['M', 'A', 'R', 'K', 'A'],
+        words: ["MAK", "RAK", "RAM", "ARK", "ARM", "KARA", "MAKA", "MARA", "RAMA", "ARAK", "AKRA", "KARMA", "MARKA"]
+    },
+    {
+        letters: ['L', 'I', 'S', 'T', 'A'],
+        words: ["LIS", "SIT", "STO", "TAL", "SILA", "LIST", "STAL", "TALI", "LITA", "SALI", "LAST", "LISTA"]
+    },
+    {
+        letters: ['P', 'R', 'A', 'C', 'A'],
+        words: ["CAP", "CAR", "PAR", "RAP", "ACRA", "CAPA", "CARA", "PARA", "RACA", "PRAC", "PRACA"]
+    },
+    {
+        letters: ['O', 'B', 'R', 'A', 'Z'],
+        words: ["BAZ", "BOA", "RAZ", "ROB", "BARO", "BAZO", "BORA", "ORAZ", "ROBA", "ZORA", "ZROB", "OBRAZ"]
+    },
+    {
+        letters: ['S', 'T', 'R', 'U', 'N'],
+        words: ["NUR", "RUS", "STU", "TUN", "NURT", "RUST", "NURS", "STRUN", "NUTS", "TURA", "SURTN", "STRUN"]
+    },
+    {
+        letters: ['K', 'A', 'N', 'A', 'Ł'],
+        words: ["KAN", "ŁAN", "NAŁ", "AKA", "KAŁA", "KANA", "NAŁA", "ŁANA", "KANAŁ"]
+    },
+    {
+        letters: ['S', 'T', 'O', 'P', 'A'],
+        words: ["PAS", "PAT", "POT", "STO", "TAP", "OSTA", "OPAT", "PASO", "POST", "POTA", "STOP", "TOPA", "STOPA"]
     }
-    // Tutaj możesz dodawać kolejne poziomy według tego samego wzoru
+
 ];
